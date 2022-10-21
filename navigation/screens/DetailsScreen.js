49035db652aa987eac88e383f7d548bf017276b1
NotArtwork <AntArce3@gmail.com>
2022-10-21 11:17:50 -0400

    created multiple screens

---
+++ navigation/screens/DetailsScreen.js
@@ -0,0 +1,18 @@
+import * as React from 'react'
+import { View, Text } from 'react-native'
+
+
+export default function DetailsScreen({ navigation }) {
+    return (
+        <View styl={{ flex: 1, alignItems: 'center', justiftContent: 'center' }}>
+            <Text
+                onPress={() => navigation.navigate('Home')}
+                style={{ fontSize: 26, fontWeight: 'bold ' }}>
+                Details
+            </Text>
+
+
+        </View>
+    )
+
+}
---
+++ navigation/screens/HomeScreen.js
@@ -0,0 +1,27 @@
+import * as React from 'react'
+import { View, Text } from 'react-native'
+
+
+export default function HomeScreen({navigation}) {
+    return(
+        <View styl={{ flex: 1, alignItems: 'center', justiftContent: 'center'}}>
+            <Text
+                onPress={() => alert('This is the "Home" screen.')}
+                style={{ fontSize: 26, fontWeight: 'bold '}}>
+                Home
+            </Text>
+
+
+        </View>
+    )
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justiftContent: 'center'
+    }
+})
